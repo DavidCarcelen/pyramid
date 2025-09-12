68c4668e2fb6e4237161ccb065875f8b33de3896
davidcarcelen <davidcarcelengenova@gmail.com>
2025-09-12 12:31:39 +0200

    key en properties

--- src/main/java/com/dcin/pyramid/security/JwtProvider.java
+++ src/main/java/com/dcin/pyramid/security/JwtProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -13,16 +14,24 @@ import java.util.Date;
 @Component
 public class JwtProvider {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.ES256);
-    private final long EXPIRATION = 1000 * 60 * 60;
+    private final Key key;
+    private final long expiration;
+
+    public JwtProvider(
+            @Value("${jwt.secret}") String secretKey,
+            @Value("${expiration}") long expiration){
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.expiration = expiration;
+    }
+
 
     public String generateToken (String email, String role){
         return Jwts.builder()
                 .setSubject(email)
                 .claim("role", role)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(key)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
     public Jws<Claims> validateToken(String token){
